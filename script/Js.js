3f32a32e4ff5a2c46fcf94ce4e3b27cb55a4bc9d
Your NameMehedi <you@example.com>
2025-03-01 15:49:54 -0600

    additional changes

--- script/Js.js
+++ script/Js.js
@@ -1,21 +1,38 @@
+document.getElementById('theme-btn').addEventListener('click', function(){
+    let color = '#';
+    color = color + Math.random().toString(16).slice(2,8);
+      document.body.style.backgroundColor= color;
+      console.log(color);
+})
+
+
+
 document.getElementById('discover-bar').addEventListener('click', (event) =>{
     event.preventDefault();
     window.location.href="discover.html"
 })
 
+
 document.querySelectorAll('#complete-btn').forEach(button => {
     button.addEventListener('click', (e) =>{
         e.preventDefault();
         alert("Board Updated Successfully");
-     
+
         updatedValueById('task-container', -1);
         updatedValueById('nav-number', 1);
+        const container = document.getElementById('activity-detail');
        
-        disableButton('complete-btn');
-        
+        const div = document.createElement('div');
+        div.innerHTML=`
+        <p>You have completed task at ${new Date().toLocaleTimeString()}</p>
+        `
+        container.appendChild(div);
+
         
     })
+  
 })
+
 
 function updatedValueById(id, value){
     const taskAssigned = document.getElementById(id).innerText;
@@ -25,6 +42,5 @@ function updatedValueById(id, value){
     
 }
 
-function disableButton(id){
-document.getElementById(id).disabled = true;
-}
+
+
