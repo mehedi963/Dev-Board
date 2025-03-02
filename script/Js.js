fb478f45ca24451a73f1ce6abc8b95b5cf4bdf4a
Your NameMehedi <you@example.com>
2025-03-02 01:20:50 -0600

    activity log complete

--- script/Js.js
+++ script/Js.js
@@ -13,34 +13,40 @@ document.getElementById('discover-bar').addEventListener('click', (event) =>{
 })
 
 
-document.querySelectorAll('#complete-btn').forEach(button => {
+document.querySelectorAll('.complete-button').forEach(button => {
     button.addEventListener('click', (e) =>{
         e.preventDefault();
         alert("Board Updated Successfully");
-
-        updatedValueById('task-container', -1);
+        
+        
+        updatedValueTaskById('task-container', 1);
         updatedValueById('nav-number', 1);
-        const container = document.getElementById('activity-detail');
+        const container = document.getElementById('activity-content');
        
         const div = document.createElement('div');
         div.innerHTML=`
         <p>You have completed task at ${new Date().toLocaleTimeString()}</p>
         `
-        container.appendChild(div);
-
-        
+        container.appendChild(div);  
     })
   
 })
-
+document.getElementById('clear-histry').addEventListener('click', () =>{
+    const activityClear = document.getElementById('activity-content');
+     console.log(activityClear)
+    activityClear.innerText = "";
+})
 
 function updatedValueById(id, value){
+    const valueAssigned = document.getElementById(id).innerText;
+    const convertedValue = parseInt(valueAssigned);
+    const sum = convertedValue + value;
+    document.getElementById(id).innerText= sum;  
+}
+function updatedValueTaskById(id, value){
     const taskAssigned = document.getElementById(id).innerText;
     const convertedValue = parseInt(taskAssigned);
-    const sum = convertedValue + value;
-    document.getElementById(id).innerText= sum;
-    
+    const sum = convertedValue - value;
+    document.getElementById(id).innerText= sum;  
 }
-
-
 
